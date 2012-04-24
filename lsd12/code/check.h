3b1beabfffebe202caf6ddfb320d5a6de65e52a2
franssenquentin@gmail.com <franssenquentin@gmail.com@c9f5b02b-3a4e-dc0a-3210-a6cf61a54b14>
2012-04-24 21:32:06 +0000

    Adding comments

--- lsd12/code/check.h
+++ lsd12/code/check.h
@@ -1,5 +1,12 @@
 /* check.h
  *
+ * File of LSD12 Compiler Project (group 11)
+ * Exercise LSD12: INFOB314/IHDCB332 : LSD12 - Group LSD12-HD-G11
+ * 
+ * Team :
+ * 	- 	Johan BARTHELEMY	johan.barthelemy@fundp.ac.be
+ * 	-	Xavier PEREMANS		xavier.peremans@student.fundp.ac.be
+ *	-	Quentin FRANSSEN	quentin.franssen@student.fundp.ac.be
  */
 
 #ifndef CHECK_H_
@@ -11,7 +18,10 @@
 #define OK 1
 #define KO 0
 
-
+/**
+ * 
+ * Pré-condiction : tree et tds ne peuvent être null, sinon le message "KO" sera renvoyé.
+ */
 int check (ASTTREE tree, SYMTABLE tds);
 
 int checkType (ASTTREE tree, int type, SYMTABLE tds);
